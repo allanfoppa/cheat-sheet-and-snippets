60a1daf085b2d5659de79e0c4db006bbedec0e6d
Allan Foppa <allanfoppa@hotmail.com>
2021-11-03 14:06:37 -0300

    Python index list

--- python/lists/index.py
+++ python/lists/index.py
@@ -5,12 +5,12 @@ def main():
 
 def find_index_by_int():
     list = [1, 5, 5, 6, 8, 10]
-    number_index = list.index(5) # Só retorna a primeira ocorrência
+    number_index = list.index(5) # Só retorna a primeira ocorrência # 1
     print(number_index)
 
 def find_index_by_string():
     list = ['Allan', 'Foppa', 'Fagundes']
-    name_index = list.index('Foppa')
+    name_index = list.index('Fagundes') # 2
     print(name_index)
 
 main()
