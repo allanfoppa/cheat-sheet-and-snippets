829641a33d81552226384687fd5d2c240513a781
Allan Foppa <allanfoppa@hotmail.com>
2024-01-01 15:17:18 -0300

    Custom hook example

---
+++ react/custom_hooks/components/Counter.tsx
@@ -0,0 +1,14 @@
+import { useCounter } from '../hooks/useCounter';
+
+export const Counter = () => {
+
+  const { count, increment, decrement } = useCounter();
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
